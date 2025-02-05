4bf3c37e89a21be30103857c37ccb520172764f1
huangjiehang <huangjiehang@gmail.com>
2025-02-06 00:07:01 +0800

    upd: Encapsulated the Client

--- src/include/client.h
+++ src/include/client.h
@@ -6,15 +6,11 @@
 #include <vector>
 #include <queue>
 #include <cstring>
+#include <cstdlib>
 
 extern int rows;         // The count of rows of the game map.
 extern int columns;      // The count of columns of the game map.
 extern int total_mines;  // The count of mines of the game map.
-
-// You MUST NOT use any other external variables except for rows, columns and total_mines.
-
-const int c_dx[] = {0, 0, 1, -1, 1, -1, 1, -1};
-const int c_dy[] = {1, -1, 0, 0, 1, -1, -1, 1};
 
 /**
  * @brief The definition of function Execute(int, int, bool)
@@ -33,13 +29,126 @@ const int c_dy[] = {1, -1, 0, 0, 1, -1, -1, 1};
  */
 void Execute(int r, int c, int type);
 
-int c_map[35][35];  // -1 - unknown, 0-8 - number of mines around, -2 - mine
-int marked_count[35][35];
-int unknown_count[35][35];
-int marked[35][35];
+namespace ClientNS {
 
-// operatoration queue - (pos, type)
-std::queue<std::pair<std::pair<int, int>, int>> op_queue;
+    const int dx[] = {0, 0, 1, -1, 1, -1, 1, -1};
+    const int dy[] = {1, -1, 0, 0, 1, -1, -1, 1};
+
+    class Client {
+    private:
+        int map[35][35];  // -1 - unknown, -2 - marked, 0-8 - number of mines 
+        int marked_count[35][35];
+        int unknown_count[35][35];
+
+        // operatoration queue - (pos, type)
+        std::queue<std::pair<std::pair<int, int>, int>> op_queue;
+        bool marked[35][35];
+
+    public:
+        Client() {
+            Reset();
+        }
+
+        void Reset() {
+            memset(map, 0, sizeof(map));
+            memset(marked_count, 0, sizeof(marked_count));
+            memset(unknown_count, 0, sizeof(unknown_count));
+            memset(marked, 0, sizeof(marked));
+            while (!op_queue.empty()) {
+                op_queue.pop();
+            }
+        }
+
+        void InitGame() {
+            int first_row, first_column;
+            std::cin >> first_row >> first_column;
+            Execute(first_row, first_column, 0);
+        }
+
+        void ReadMap() {
+            memset(marked_count, 0, sizeof(marked_count));
+            memset(unknown_count, 0, sizeof(unknown_count));
+            for (int i = 0; i < rows; ++i) {
+                for (int j = 0; j < columns; ++j) {
+                    char c;
+                    std::cin >> c;
+                    if (c == '?') {
+                        // The block is unknown.
+                        map[i][j] = -1;
+                        for (int k = 0; k < 8; ++k) {
+                            int x = i + dx[k];
+                            int y = j + dy[k];
+                            if (x >= 0 && x < rows && y >= 0 && y < columns) {
+                                ++unknown_count[x][y];
+                            }
+                        }
+                    } else if (c == '@') {
+                        // The block has been marked as a mine.
+                        map[i][j] = -2;
+                        for (int k = 0; k < 8; ++k) {
+                            int x = i + dx[k];
+                            int y = j + dy[k];
+                            if (x >= 0 && x < rows && y >= 0 && y < columns) {
+                                ++marked_count[x][y];
+                            }
+                        }
+                    } else {
+                        // The block has been visited.
+                        map[i][j] = c - '0';
+                    }
+                }
+            }
+        }
+
+        void SimpleDetect() {
+            for (int i = 0; i < rows; ++i) {
+                for (int j = 0; j < columns; ++j) {
+                    if (map[i][j] > 0 && marked_count[i][j] + unknown_count[i][j] == map[i][j]) {
+                        for (int k = 0; k < 8; ++k) {
+                            int x = i + dx[k];
+                            int y = j + dy[k];
+                            if (x >= 0 && x < rows && y >= 0 && y < columns &&
+                                !marked[x][y] && map[x][y] == -1) {
+                                op_queue.push({{x, y}, 1});
+                                marked[x][y] = true;
+                            }
+                        }
+                    }
+                    if (map[i][j] > 0 && marked_count[i][j] == map[i][j] && unknown_count[i][j] > 0) {
+                        op_queue.push({{i, j}, 2});
+                    }
+                }
+            }
+        }
+
+        void Decide() {
+            if (op_queue.empty()) {
+                SimpleDetect();
+            }
+            if (!op_queue.empty()) {
+                auto front = op_queue.front();
+                op_queue.pop();
+                int r = front.first.first;
+                int c = front.first.second;
+                int type = front.second;
+                Execute(r, c, type);
+            } else {
+                int x, y;
+                do {
+                    x = rand() % rows;
+                    y = rand() % columns;
+                } while (map[x][y] != -1);
+                Execute(x, y, 0);
+            }
+        }
+
+    };
+}
+
+inline ClientNS::Client& getClientInstance() {
+    static ClientNS::Client client;
+    return client;
+}
 
 /**
  * @brief The definition of function InitGame()
@@ -48,14 +157,8 @@ std::queue<std::pair<std::pair<int, int>, int>> op_queue;
  * will read the scale of the game map and the first step taken by the server (see README).
  */
 void InitGame() {
-    // TODO (student): Initialize all your global variables!
-    memset(c_map, 0, sizeof(c_map));
-    memset(marked_count, 0, sizeof(marked_count));
-    memset(unknown_count, 0, sizeof(unknown_count));
-    op_queue = std::queue<std::pair<std::pair<int, int>, int>>();
-    int first_row, first_column;
-    std::cin >> first_row >> first_column;
-    Execute(first_row, first_column, 0);
+    getClientInstance().Reset();
+    getClientInstance().InitGame();
 }
 
 /**
@@ -69,59 +172,7 @@ void InitGame() {
  *     01?
  */
 void ReadMap() {
-    memset(marked_count, 0, sizeof(marked_count));
-    memset(unknown_count, 0, sizeof(unknown_count));
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < columns; ++j) {
-            char c;
-            std::cin >> c;
-            if (c == '?') {
-                // The block is unknown.
-                c_map[i][j] = -1;
-                for(int k = 0; k < 8; ++k) {
-                    int x = i + c_dx[k];
-                    int y = j + c_dy[k];
-                    if(x >= 0 && x < rows && y >= 0 && y < columns) {
-                        ++unknown_count[x][y];
-                    }
-                }
-            } else if(c == '@'){
-                // The block has been marked.
-                c_map[i][j] = -2;
-                for(int k = 0; k < 8; ++k) {
-                    int x = i + c_dx[k];
-                    int y = j + c_dy[k];
-                    if(x >= 0 && x < rows && y >= 0 && y < columns) {
-                        ++marked_count[x][y];
-                    }
-                }
-            } else {
-                // The block has been visited.
-                c_map[i][j] = c - '0';
-            }
-        }
-    }
-}
-
-// some point may be added multiple times, but it doesn't matter
-void SimpleDectect() {
-    for(int i = 0; i < rows; ++i) {
-        for(int j = 0; j < columns; ++j) {
-            if(c_map[i][j] > 0 && marked_count[i][j] + unknown_count[i][j] == c_map[i][j]) {
-                for(int k = 0; k < 8; ++k) {
-                    int x = i + c_dx[k];
-                    int y = j + c_dy[k];
-                    if(x >= 0 && x < rows && y >= 0 && y < columns && !marked[x][y] && c_map[x][y] == -1) {
-                        op_queue.push({{x, y}, 1});
-                        marked[x][y] = true;
-                    }
-                }
-            }
-            if(c_map[i][j] > 0 && marked_count[i][j] == c_map[i][j] && unknown_count[i][j] > 0) {
-                op_queue.push({{i, j}, 2});
-            }
-        }
-    }
+    getClientInstance().ReadMap();
 }
 
 /**
@@ -130,29 +181,8 @@ void SimpleDectect() {
  * @details This function is designed to decide the next step when playing the client's (or player's) role. Open up your
  * mind and make your decision here!
  */
-
-/*
-first: simple detect
-second: randomly
-*/
-
 void Decide() {
-    if(op_queue.empty()) {
-        SimpleDectect();
-    }
-    if(!op_queue.empty()) {
-        auto [pos, type] = op_queue.front();
-        op_queue.pop();
-        Execute(pos.first, pos.second, type);
-    } else {
-        // randomly choose a point (unknow) to visit
-        int x, y;
-        do {
-            x = rand() % rows;
-            y = rand() % columns;
-        } while(c_map[x][y] != -1);
-        Execute(x, y, 0);
-    }
+    getClientInstance().Decide();
 }
 
-#endif
+#endif // CLIENT_H
